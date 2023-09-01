23c048f7239e768b59007b5d42b9d70d49850e12
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-01 18:17:44 +0100

    Completed assginemnt

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // The goal --> Creating a program that will print only the words that have the same meaning if it was read from begging to the end and from the end to the begging.
+  // task: Create a function that will check the words if it meet the conditions 
+  // call the function every time the user clicks on "check the word" button
+  const spli = word.split('')
+  const anothere = spli.reverse('')
+  const okd = anothere.join('')
+  if (okd === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
 
+  locate the input and give it a name
+  
+  make a function that checks the value of the input
+  if the value of input could be read from start to end and from end to start 
+  then return true
+  else return false
+*/
 /*
   Add written explanation of your solution here
+  the code checks if the value of the input could be read from start to end, if it does then the program returns true and if its not, the program will return false  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log('=>', isPalindrome('monom'));
+
+  console.log('')
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('copypast'))
+
+  console.log('')
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
